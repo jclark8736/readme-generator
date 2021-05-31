213d73174e2374e32ed6198f14d88ec71acfd70b
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-31 16:25:11 -0400

    git ignore

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # readme-generator
+
+## project name #${data.title}
+
+
+## user email #${data.email}
+
+## Github Username #${data.github}
+
+*list item one
+*list item two
+
+    -example one
+    -example two
+
+
+[installation link]
+
+
+## How to run application
+    -npm i
---
+++ index2.js
@@ -0,0 +1,44 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer")
+const path = require("path");
+const generateMarkdown= require("./utils/generateMarkdown.js")
+
+// TODO: Create an array of questions for user input
+const questions = [
+    //example question format types - input, list etc - look up types in inquirer docs - list takes an array of choices user selects
+    {
+    type: "input",
+    name: "github",
+    message: "What is your github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+        },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your application's title?"
+    },
+   
+];
+
+// TODO: Create a function to write README file using user input from our choices above
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+
+// TODO: Create a function to initialize app (by calling inquirer)
+function init() {
+    inquirer.prompt(questions).then(
+        (response) => {
+            console.log("Your README is being created")
+            writeToFile("README.md". generateMarkdown(...response))
+        }
+    )
+}
+
+// Function call to initialize app
+init();
