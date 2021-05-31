a4af85e9982f696fa53302fb90e29d53ab23bb52
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-31 18:28:24 -0400

    basic outline of html elements

--- index2.js
+++ index2.js
@@ -14,73 +14,73 @@ const questions = [
     //example question format types - input, list etc - look up types in inquirer docs - list takes an array of choices user selects
     {
         type: "input",
-        name: "name",
+        name: "name",//
         message: "What is your name?",
     },
     {
         type: "input",
-        name: "github",
+        name: "github",//
         message: "What is your github username?",
     },
     {
         type: "input",
-        name: "email",
+        name: "email",//
         message: "What is your email address?",
         },
     {
         type: "input",
-        name: "title",
+        name: "title",//
         message: "What is your application's title?",
     },
     {
         type: "input",
-        name: "images",
+        name: "images",//
         message: "Please submit any links to images or videos you would like to place within your readme file, separated by comma",
     },
     {
         type: "input",
-        name: "objective",
+        name: "objective",//
         message: "What objective does this application seek to achieve?",
     },
     {
         type: "input",
-        name: "userstory",
+        name: "userstory",//
         message: "What is the user story associated with this application?",
     },
     {
         type: "checkbox",
-        name: "technologies",
+        name: "technologies",//
         message: "What technologies are used in this application?",
         choices: ["javascript", "HTML", "CSS", "node.js", "inquirer", "git"],
     },
     {
         type: "input",
-        name: "tech",
+        name: "tech",//
         message: "Please list any additional technologies used, delineated by commas.",
     },
     {
         type: "input",
-        name: "installation",
+        name: "installation",//
         message: "Please write out installation instructions necessary to install this program",
     },
     {
         type: "input",
-        name: "usage",
+        name: "usage",//
         message: "Please write instructions on how to run this application",
     },
     {
         type: "input",
-        name: "credits",
+        name: "credits",//
         message: "List any groups or individuals who assisted you in the development of this application",
     },
     {
         type: "input",
-        name: "testing",
+        name: "testing",//
         message: "Please write out any applicable testing information",
     },
     {
         type: "checkbox",
-        name: "licenses",
+        name: "licenses",//
         message: "What license would you like to use for this application?",
         choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", "BSD 3-clause Clear license",
         "Creative Commons license family", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0",
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,29 +1,62 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license !== "None") {
-    else return `![Github License](https...)`
-  }
-  return ""``
-}
+// // //else return `![Github License](https...)`
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // // TODO: Create a function that returns a license badge based on which license is passed in
+// // // If there is no license, return an empty string
+// // function renderLicenseBadge(license) {
+// //   if (license !== "None") {
+// //     return
+    
+// //   }
+// //   return ""``
+// // }
 
-//if else
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-//if else
+// //if else
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+// //if else
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title};
+  ## Objectives and Description:
+  ${data.objective}
+  ${data.userstory}
 
 
-`;
+  ##Technologies Used
+  ${data.technologies}
+  ${data.tech}
+
+  ##Installation Instructions
+  ${data.installation}
+
+  ##Usage Instructions
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+  ## Licenses
+  ${data.licenses}
+
+  ## Features
+  ${data.images}
+
+  ## How to contribute - please contact
+  ${data.name} ${data.email} ${data.github}
+
+  ##Testing
+  ${data.testing}
+
+`
 }
 
 module.exports = generateMarkdown;
